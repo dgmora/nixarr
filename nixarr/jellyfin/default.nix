89c1cc5c9792f162b7d761ff76eb369dd4d96aa0
David Garcia <dgmora.dev@gmail.com>
2026-09-10 20:58:49 +0200

    feat: create Jellyfin libraries across media dirs

--- nixarr/jellyfin/default.nix
+++ nixarr/jellyfin/default.nix
@@ -9,6 +9,7 @@ with lib; let
   globals = config.util-nixarr.globals;
   defaultPort = 8096;
   nixarr = config.nixarr;
+  managedMediaDirs = map (media: media.path) (filter (media: media.create) nixarr.mediaDirs);
 in {
   options.nixarr.jellyfin = {
     enable = mkOption {
@@ -172,21 +173,22 @@ in {
       };
     };
 
-    systemd.tmpfiles.rules = [
-      "d '${cfg.stateDir}'        0700 ${globals.jellyfin.user} root - -"
-      "d '${cfg.stateDir}/log'    0700 ${globals.jellyfin.user} root - -"
-      "d '${cfg.stateDir}/cache'  0700 ${globals.jellyfin.user} root - -"
-      "d '${cfg.stateDir}/data'   0700 ${globals.jellyfin.user} root - -"
-      "d '${cfg.stateDir}/config' 0700 ${globals.jellyfin.user} root - -"
-
-      # Media Dirs
-      "d '${nixarr.mediaDir}/library'             0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
-      "d '${nixarr.mediaDir}/library/shows'       0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
-      "d '${nixarr.mediaDir}/library/movies'      0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
-      "d '${nixarr.mediaDir}/library/music'       0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
-      "d '${nixarr.mediaDir}/library/books'       0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
-      "d '${nixarr.mediaDir}/library/audiobooks'  0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
-    ];
+    systemd.tmpfiles.rules =
+      [
+        "d '${cfg.stateDir}'        0700 ${globals.jellyfin.user} root - -"
+        "d '${cfg.stateDir}/log'    0700 ${globals.jellyfin.user} root - -"
+        "d '${cfg.stateDir}/cache'  0700 ${globals.jellyfin.user} root - -"
+        "d '${cfg.stateDir}/data'   0700 ${globals.jellyfin.user} root - -"
+        "d '${cfg.stateDir}/config' 0700 ${globals.jellyfin.user} root - -"
+      ]
+      ++ concatMap (mediaDir: [
+        "d '${mediaDir}/library'             0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
+        "d '${mediaDir}/library/shows'       0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
+        "d '${mediaDir}/library/movies'      0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
+        "d '${mediaDir}/library/music'       0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
+        "d '${mediaDir}/library/books'       0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
+        "d '${mediaDir}/library/audiobooks'  0775 ${globals.libraryOwner.user} ${globals.libraryOwner.group} - -"
+      ]) managedMediaDirs;
 
     # Always prioritise Jellyfin IO
     systemd.services.jellyfin.serviceConfig.IOSchedulingPriority = 0;
